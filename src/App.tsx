54caa8d6158814110fd0c3e1050dbe3d8c72fca6
Fafemi Adeola <adeola5678@gmail.com>
2021-10-19 09:34:16 +0100

    basic login with useDApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
+import Layout from "./components/Layout";
+import ConnectButton from "./components/connectBotton";
+import AccountModal from "./components/accountModal";
 
-function App() {
+export default function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Layout>
+        <ConnectButton handleOpenModal={onOpen} />
+        <AccountModal isOpen={isOpen} onClose={onClose} />
+      </Layout>
+    </ChakraProvider>
   );
 }
-
-export default App;
